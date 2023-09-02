e3b58b8795eecf0fe7e49805197fe6da997d12c1
jaybee08 <montejo.jaybee@gmail.com>
2023-09-03 01:03:23 +0800

    update if conditioni

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,11 @@ const Shopify = require('shopify-api-node');
 const express = require('express');
 const cors = require('cors');
 const fetchProductsFromShopify = require('./api/product');
-const fetchUSDtoPHPExchangeRate = require('./usd-php');
 const axios = require('axios');
-const puppeteer = require('puppeteer'); // Import Puppeteer module
-const bodyParser = require('body-parser');
+const fetchUSDtoPHPExchangeRate = require('./usd-php'); 
+
 
 const app = express();
-app.use(bodyParser.json());
 
 // Set up CORS headers to allow requests from your frontend
 app.use(cors());
@@ -21,12 +19,10 @@ const shopify = new Shopify({
   password: process.env.SHOPIFY_PASSWORD,
 });
 
-
 // Fetch shopify product information
 app.get('/api/product', async (req, res) => {
   try {
     const products = await fetchProductsFromShopify();
-
     // Send the fetched product information to the frontend
     res.json(products);
   } catch (error) {
@@ -35,76 +31,65 @@ app.get('/api/product', async (req, res) => {
   }
 });
 
+const WEBHOOK_PROCESS_INTERVAL = 120 * 1000; // 120 seconds
+let lastPriceUpdateTimestamp = null;
 
-// webhook update price/calculate
 app.post('/webhooks/shopify', async (req, res) => {
   try {
-    // Handle the incoming webhook data
-    const webhookData = req.body; // Contains the webhook payload
+    // Check the X-Shopify-Topic header to determine the type of webhook
+    const webhookTopic = req.headers['x-shopify-topic'];
+    const webhookData = req.body;
 
-    // Extract the necessary information from the webhook payload
-    const { id, variants, event, product_id } = webhookData; // Example: Product ID, variants, event, and product ID
+    if (webhookTopic === 'products/create') {
+      console.log('Received product creation webhook.');
+      // Process product creation here
+    } else if (webhookTopic === 'products/update') {
+      // Check if the webhook is specifically for a price update
+      const myCustomHeader = req.headers['my-custom-header'];
+      if (myCustomHeader !== 'price_update') {
+        const currentTime = new Date().getTime();
+        console.log('price update wehbook triggered');
+        if (!lastPriceUpdateTimestamp || (currentTime - lastPriceUpdateTimestamp >= WEBHOOK_PROCESS_INTERVAL)) {
+          lastPriceUpdateTimestamp = currentTime;
+          // Handle price update logic here
+          console.log('Received price update webhook.');
+					
+					// Fetch USD to PHP exchange rate
+					let usdToPhpExchangeRate;
+					try {
+						usdToPhpExchangeRate = await fetchUSDtoPHPExchangeRate();
+					} catch (error) {
+						console.error('Error fetching USD to PHP exchange rate:', error.message);
+						return res.status(500).json({ error: 'An error occurred while fetching USD to PHP exchange rate' });
+					}
 
-    // Check if the webhook is for a new product creation event
-    if (event === 'products/create') {
-      const variant = variants[0]; // Consider the first variant, adjust as needed
-      const usdToPhpExchangeRate = await fetchUSDtoPHPExchangeRate();
+          // Extract necessary data from webhook payload
+          const { variants, id } = webhookData;
+          const variant = variants[0]; // Consider the first variant, adjust as needed
 
-      // Calculate the new price in PHP
-      const updatedPriceUSD = parseFloat(variant.price);
-      const updatedPricePHP = (updatedPriceUSD * usdToPhpExchangeRate).toFixed(2);
+          // Calculate the new price in PHP
+          const updatedPriceUSD = parseFloat(variant.price);
+          const updatedPricePHP = (updatedPriceUSD * usdToPhpExchangeRate).toFixed(2);
 
-      // Update the variant price in Shopify using a GraphQL mutation
-      const mutation = `
-        mutation {
-          productVariantUpdate(input: {
-            id: "gid://shopify/ProductVariant/${variant.id}",
-            price: "${updatedPricePHP}"
-          }) {
-            productVariant {
-              id
-              title
-            }
-          }
-        }
-      `;
-
-      try {
-        const response = await axios.post(
-          process.env.SHOPIFY_GRAPHQL_URL,
-          { query: mutation },
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              'X-Shopify-Access-Token': process.env.SHOPIFY_API_TOKEN,
-            },
-          }
-        );
-
-        const responseData = response.data;
-
-        if (responseData.errors) {
-          console.error('Error updating variant price:', responseData.errors);
-        } else if (responseData.data && responseData.data.productVariantUpdate) {
-          const updatedVariant = responseData.data.productVariantUpdate.productVariant;
-          console.log('Updated Variant:', updatedVariant);
-
-          // Add a "price_converted" tag to the product
-          const tagMutation = `
+          // Update the price in Shopify using a GraphQL mutation
+          const mutation = `
             mutation {
-              tagsAdd(id: "gid://shopify/Product/${product_id}", tags: "price_converted") {
-                node {
+              productVariantUpdate(input: {
+                id: "gid://shopify/ProductVariant/${variant.id}",
+                price: "${updatedPricePHP}"
+              }) {
+                productVariant {
                   id
-                  tags
+                  title
                 }
               }
             }
           `;
 
           try {
-            const tagResponse = await axios.post(
+            const response = await axios.post(
               process.env.SHOPIFY_GRAPHQL_URL,
-              { query: tagMutation },
+              { query: mutation },
               {
                 headers: {
                   'Content-Type': 'application/json',
@@ -113,20 +98,26 @@ app.post('/webhooks/shopify', async (req, res) => {
               }
             );
 
-            const tagData = tagResponse.data;
-            if (tagData.errors) {
-              console.error('Error adding tag:', tagData.errors);
-            } else if (tagData.data && tagData.data.tagsAdd) {
-              console.log('Tag added:', tagData.data.tagsAdd.node);
+            const responseData = response.data;
+
+            if (responseData.errors) {
+              console.error('Error updating variant price:', responseData.errors);
+            } else if (responseData.data && responseData.data.productVariantUpdate) {
+              const updatedVariant = responseData.data.productVariantUpdate.productVariant;
+              console.log('Updated Variant:', updatedVariant);
+            } else {
+              console.error('Unexpected response format:', responseData);
             }
-          } catch (tagError) {
-            console.error('Error adding tag:', tagError);
+          } catch (error) {
+            console.error('Error making request:', error);
           }
+
         } else {
-          console.error('Unexpected response format:', responseData);
+          console.log('Received price update webhook within time interval, skipping processing.');
         }
-      } catch (error) {
-        console.error('Error making request:', error);
+      } else {
+        console.log('Received regular product update webhook.');
+        // Process regular product update here
       }
     }
 
@@ -137,7 +128,6 @@ app.post('/webhooks/shopify', async (req, res) => {
     res.status(500).json({ error: 'An error occurred while processing the webhook.' });
   }
 });
-
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
